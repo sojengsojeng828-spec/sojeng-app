8f9dff5f9a2dc703daa5895cd697721d04f92dfb
sojengsojeng828-spec <sojengsojeng828@gmail.com>
2026-06-29 14:52:28 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://fzutuwlsbayatkcmjbmo.supabase.co'
+const supabaseUrl = 'https://kdmontqhiugkjkakmy kq.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkbW9udHFoaXVna2prYWtteWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1NDU1NDMsImV4cCI6MjA5ODEyMTU0M30.ZrY4_rCdb-PPr-ljINn2e8Pp5bhRF46MTqHCqTONIGM'
 
-export const supabase = supabaseKey ? createClient(supabaseUrl, supabaseKey) : null
-export const isSupabaseReady = !!supabaseKey
+export const supabase = createClient(supabaseUrl, supabaseKey)
+export const isSupabaseReady = true
